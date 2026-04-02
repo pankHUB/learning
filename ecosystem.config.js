d9cd10cb0d87aa95a5f34f19558b88d8215584f4
pankHUB <pankulrathi@gmail.com>
2026-04-02 15:52:09 +0530

    add deployment yml

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,13 @@
-module.exports = {
-  apps: [
-    {
-      name: 'nextjs-app',
-      script: 'node_modules/.bin/next',
-      args: 'start',
-      cwd: '/home/deployer/app',
-      interpreter: '/home/deployer/.nvm/versions/node/v20.x.x/bin/node',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3000,
-      },
+export const apps = [
+  {
+    name: 'nextjs-app',
+    script: 'node_modules/.bin/next',
+    args: 'start',
+    cwd: '/home/deployer/app',
+    interpreter: '/home/deployer/.nvm/versions/node/v20.x.x/bin/node',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000,
     },
-  ],
-}
+  },
+];
